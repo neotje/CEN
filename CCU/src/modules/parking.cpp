93c3b5dabfa5277c3fccbc0e02b98c2c18bcecbf
Neo Hop <neotje111@gmail.com>
2021-06-30 11:03:41 +0200

    added intervaltimer

--- CCU/src/modules/parking.cpp
+++ CCU/src/modules/parking.cpp
@@ -4,17 +4,24 @@ uint8_t ParkingBeeper::beeperPin = BEEPER_PIN;
 uint32_t ParkingBeeper::_min = MIN_DISTANCE;
 uint32_t ParkingBeeper::_max = MAX_DISTANCE;
 bool ParkingBeeper::enable = true;
+IntervalTimer ParkingBeeper::beeper_timer = IntervalTimer();
 
 void ParkingBeeper::setup()
 {
     pinMode(beeperPin, OUTPUT);
 }
 
+static bool running = false;
+
 void ParkingBeeper::loop()
 {
     // check if parkingbeeper is enabled and the car is in reverse
-    if (enable && ShifterSensor.get_gear() == SHIFTER_SPEEDS - 1)
+    if (enable && ShifterSensor.get_gear() == SHIFTER_SPEEDS - 1 && !running)
     {
         
+    }
+    else
+    {
+        beeper_timer.end();
     }
 }
--- CCU/src/modules/parking.h
+++ CCU/src/modules/parking.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <Arduino.h>
+#include <IntervalTimer.h>
+
 #include "../config.h"
 #include "../sensors.h"
 
@@ -10,8 +12,11 @@ private:
     static uint8_t beeperPin;
     static uint32_t _max;
     static uint32_t _min;
+    static IntervalTimer beeper_timer;
+
 public:
     static bool enable;
+    static uint16_t frequency;
 
     static void setup();
     static void loop();
