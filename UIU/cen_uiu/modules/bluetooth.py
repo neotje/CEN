101c57a5df7cb3c7227ded610b5db573097c039b
neotje <neotje111@gmail.com>
2021-07-09 09:28:14 +0200

    added tests

--- UIU/cen_uiu/modules/bluetooth.py
+++ UIU/cen_uiu/modules/bluetooth.py
@@ -1,16 +1,25 @@
 import subprocess
 
+
 def discoverable(enable: bool = True):
     if enable:
-        subprocess.run(["bluetoothctl", "discoverable", "on"])
+        result = subprocess.run(["bluetoothctl", "discoverable", "on"])
     else:
-        subprocess.run(["bluetoothctl", "discoverable", "off"])
+        result = subprocess.run(["bluetoothctl", "discoverable", "off"])
+
+    if result.returncode != 0:
+        raise BluetoothError
+
+    return True
+
 
 def set_alias(alias: str):
     result = subprocess.run(["bluetoothctl", "system-alias", alias])
 
-    if result.returncode is not 0:
+    if result.returncode != 0:
         raise BluetoothError
+
+    return True
 
 
 class BluetoothError(Exception):
---
+++ UIU/tests/bluetooth.test.py
@@ -0,0 +1,23 @@
+from cen_uiu.modules import bluetooth, audio
+
+
+def test_discoverable():
+    assert bluetooth.discoverable() is True
+    assert bluetooth.discoverable(False) is True
+
+
+def test_set_alias():
+    assert bluetooth.set_alias("Matiz") is True
+
+
+def test_BluetoothInput():
+    bl = audio.BluetoothInput()
+
+    assert bl.enable() == None
+    assert bl.disable() == None
+
+
+if __name__ == "__main__":
+    test_discoverable()
+    test_set_alias()
+    print("Bluetooth passed")
