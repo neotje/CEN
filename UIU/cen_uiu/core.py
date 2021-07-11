5731bc2d4370ee6f232a3ac29e8f78b394e46c2d
Neo Hop <neotje111@gmail.com>
2021-07-11 20:23:58 +0200

    Updated README.md

--- UIU/cen_uiu/core.py
+++ UIU/cen_uiu/core.py
@@ -8,9 +8,6 @@ from cen_uiu.modules.bluetooth import BL_ON_CONNECT_EVENT, BluetoothDiscovery
 from cen_uiu.update import UpdateThread
 from cen_uiu.worker import UIUCoreWorker
 
-from cen_uiu.__main__ import exit_code
-
-
 from kivy.logger import Logger
 _LOGGER = Logger
 
