7c0455598c4786bed69a9311f7e082ddf78a7df0
Neo Hop <neotje111@gmail.com>
2021-07-11 21:47:00 +0200

    default window size

--- UIU/cen_uiu/app.py
+++ UIU/cen_uiu/app.py
@@ -3,10 +3,10 @@ from kivy.uix.widget import Widget
 
 from kivy.config import Config
 
-Config.set('graphics', 'width', '200')
-Config.set('graphics', 'height', '200')
-Config.set('graphics', 'window_state', 'hidden')
-
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '480')
+Config.set('graphics', 'window_state', 'visible')
+Config.set('graphics', 'resizable', False)
 
 class Root(Widget):
     def __init__(self, **kwargs):
