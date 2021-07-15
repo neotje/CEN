ccc8920e004584de859acf2cea54728e8fab2962
Neo Hop <neotje111@gmail.com>
2021-07-15 22:55:43 +0200

    screen manager listens to to switch to screen event

--- UIU/cen_uiu/gui/MainScreenManager.py
+++ UIU/cen_uiu/gui/MainScreenManager.py
@@ -1,4 +1,5 @@
 
+from cen_uiu.event import EventManager, SWITCH_TO_SCREEN
 from cen_uiu.gui.HomeScreen import HomeScreen
 from kivy.uix.screenmanager import ScreenManager
 
@@ -8,3 +9,13 @@ class MainScreenManager(ScreenManager):
         super(MainScreenManager, self).__init__()
 
         self.add_widget(HomeScreen())
+
+        EventManager.listen(
+            SWITCH_TO_SCREEN, lambda data: self._switch_to_event(data))
+
+    def _switch_to_event(self, data: dict):
+        name = data.get("screen")
+
+        for screen in self.screens:
+            if screen.name == name:
+                self.switch_to(screen)
