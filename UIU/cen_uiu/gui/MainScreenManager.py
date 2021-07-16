31284d0f7fcf71b2892edca5587fa0280d531ff1
Neo Hop <neotje111@gmail.com>
2021-07-16 10:20:45 +0200

    added updatescreen

--- UIU/cen_uiu/gui/MainScreenManager.py
+++ UIU/cen_uiu/gui/MainScreenManager.py
@@ -1,6 +1,7 @@
 
-from cen_uiu.event import EventManager, SWITCH_TO_SCREEN
+from cen_uiu.event import EventManager, ON_SCREEN_SWITCH, SWITCH_TO_SCREEN
 from cen_uiu.gui.HomeScreen import HomeScreen
+from cen_uiu.gui.UpdateScreen import UpdateScreen
 from kivy.uix.screenmanager import ScreenManager
 
 
@@ -9,6 +10,7 @@ class MainScreenManager(ScreenManager):
         super(MainScreenManager, self).__init__()
 
         self.add_widget(HomeScreen())
+        self.add_widget(UpdateScreen())
 
         EventManager.listen(
             SWITCH_TO_SCREEN, lambda data: self._switch_to_event(data))
@@ -18,4 +20,5 @@ class MainScreenManager(ScreenManager):
 
         for screen in self.screens:
             if screen.name == name:
+                EventManager.dispatch(ON_SCREEN_SWITCH, {'screen': name})
                 self.switch_to(screen)
---
+++ UIU/cen_uiu/gui/UpdateScreen.py
@@ -0,0 +1,24 @@
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_string('''
+#:import utils kivy.utils
+
+<UpdateScreen>:
+    canvas.before:
+        Color:
+            rgb: utils.get_color_from_hex("#03adfc")
+        Rectangle:
+            size: self.size
+    
+    BoxLayout:
+        Label:
+            font_size: 30
+            text: "Updaten..."
+''')
+
+class UpdateScreen(Screen):
+    def __init__(self, **kw):
+        super(UpdateScreen, self).__init__(**kw)
+
+        self.name = "update"
