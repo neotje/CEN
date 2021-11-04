22978cda8b07ae401ae877b911ba5a9dd236902b
neotje <neotje111@gmail.com>
2021-11-04 14:22:01 +0100

    added shared theme options between dark and light

--- UIU/frontend/src/components/theme/themes.js
+++ UIU/frontend/src/components/theme/themes.js
@@ -1,30 +1,39 @@
 import { createTheme } from "@material-ui/core/styles"
 
-const colors = {
-    primary: {
-        light: '#5e92f3',
-        main: '#1565c0',
-        dark: '#003c8f',
-        contrastText: '#fff',
+const shared = createTheme({
+    palette: {
+        primary: {
+            light: '#5e92f3',
+            main: '#1565c0',
+            dark: '#003c8f',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ffc046',
+            main: '#ff8f00',
+            dark: '#c56000',
+            contrastText: '#000',
+        },
     },
-    secondary: {
-        light: '#ffc046',
-        main: '#ff8f00',
-        dark: '#c56000',
-        contrastText: '#000',
+    typography: {
+        fontSize: 18
     },
-}
+    spacing: 8,
+    shape: {
+        borderRadius: 6
+    }
+})
 
 const themes = {
     light: createTheme({
+        ...shared,
         palette: {
-            ...colors,
             type: "light"
         }
     }),
     dark: createTheme({
+        ...shared,
         palette: {
-            ...colors,
             type: "dark"
         },
     })
