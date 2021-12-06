b4bc92ca8f4d4b0bf441e82cb01f04a4a0de37d3
Neo Hop <neotje111@gmail.com>
2021-12-06 14:36:24 +0100

    linear interpolation color

--- modules/FrontLedCan/effect.py
+++ modules/FrontLedCan/effect.py
@@ -2,7 +2,14 @@ from ledManager import LedManager
 import math
 
 def lerp(start, stop, amt):
-    return start + (stop - start)*amt
+    return (stop - start)*amt + start
+
+def lerpColor(c1, c2, amt):
+    return (
+        round(lerp(c1[0], c2[0], amt)),
+        round(lerp(c1[1], c2[1], amt)),
+        round(lerp(c1[2], c2[2], amt))
+    )
 
 class Effect:
     _id: int
@@ -28,11 +35,7 @@ class FillEffect(Effect):
         target = pallete[0]
 
 
-        self.currentColor = (
-            lerp(self.currentColor[0], target[0], dt * self.speed),
-            lerp(self.currentColor[1], target[1], dt * self.speed),
-            lerp(self.currentColor[2], target[2], dt * self.speed)
-        )
+        self.currentColor = lerpColor(self.currentColor, target, 0.05)
 
         self.leds.fill(self.currentColor)
         
